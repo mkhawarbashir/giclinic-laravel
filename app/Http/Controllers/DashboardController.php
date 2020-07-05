bd9086d24a8db6afec3e059c89552361e9ea59c7
mnahmed77 <mnahmed77@gmail.com>
2020-07-05 22:46:22 +0200

    DashboardController resent after adding space to the version from 28.06.2020

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;   //Added User model to DashboardController. [MN - 28.06.2020]
+use App\User;   //Added User model to DashboardController. [MN - 28.06.2020] 
 
 class DashboardController extends Controller
 {
